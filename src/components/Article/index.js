ab424d20b1210fc8caf124d89498938f316cf1ed
Aurèle Oulès <aurele@oules.com>
2019-03-30 14:36:20 +0100

    description & relative date

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,12 +2,19 @@ import React from 'preact';
 
 import './styles.scss';
 import { Link } from 'preact-router/match';
+import removeMd from 'remove-markdown';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime'
 
+dayjs.extend(relativeTime)
 class Article extends React.Component {
+
     render() {
         return (
             <Link className="article-box-container" href={"/article/" + this.props.article.short_id}>
-                {this.props.article.title}
+                <h1>{this.props.article.title}</h1>
+                <p>{removeMd(this.props.article.content.substr(0, 100))}...</p>
+                <span>{dayjs(this.props.article.created_at).fromNow()}</span>
             </Link>
         )
     }
